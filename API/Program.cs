a3e95ebbfb96f4498918e2cedbb63abf5b5b6a67
Vandesh Prakash Kadge <vandesh.kadge@newgen.co.in>
2022-01-03 20:47:40 +0530

    End of Section 1

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>{
+opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Scope 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+ var context = services.GetRequiredService<DataContext>(); 
+ context.Database.Migrate(); 
+ await Seed.SeedData(context);
+
+}
+catch (Exception ex)
+{
+    
+   var logger = services.GetRequiredService<ILogger<Program>>();
+   logger.LogError(ex,"An Error occured during migration");
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
